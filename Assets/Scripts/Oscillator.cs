b70c819c3f3ba028363d19cdff376e58af90cc3d
Adam Pospíšek <adam.pospisek@email.cz>
2022-08-03 21:27:09 +0200

    improved moving obstacles with code

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,8 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 2f;
 
     void Start()
     {
@@ -16,6 +17,12 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period; // continually growing over time
+        const float tau = Mathf.PI * 2; // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1
+        
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
