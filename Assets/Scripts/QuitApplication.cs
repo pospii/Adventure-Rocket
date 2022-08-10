325111427b7ea1f63cd00cbeba1d4418c4287762
Adam Pospíšek <adam.pospisek@email.cz>
2022-08-10 23:19:54 +0200

    new levels

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
+    }
+}
